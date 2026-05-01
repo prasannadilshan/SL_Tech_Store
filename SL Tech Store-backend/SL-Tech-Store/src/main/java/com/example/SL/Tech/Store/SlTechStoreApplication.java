af8484deba1565ce73c9da54599e83337f3eb80c
Prasanna Dilshan <prasannadilshan2002@gmail.com>
2026-05-01 15:52:26 +0530

    first commit

---
+++ SL Tech Store-backend/SL-Tech-Store/src/main/java/com/example/SL/Tech/Store/SlTechStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.SL.Tech.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlTechStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlTechStoreApplication.class, args);
+	}
+
+}
